224b5f78448ac0e7a194a6b2b8bdb6b825ce4cfd
Moses Johansson <moses.johansson@preciofishbone.se>
2020-07-30 15:28:24 +0200

    remove redudant css since we never render nonactive images

--- src/components/MosesImageCarousel.tsx
+++ src/components/MosesImageCarousel.tsx
@@ -5,72 +5,98 @@ import './MosesImageCarousel.css';
 import * as React from 'react';
 import SlideImage from '../Models/SlideImage.model';
 import { time } from 'console';
+import SlideDot from '../Models/SlideDot.model';
+import { domainToASCII } from 'url';
 
 export interface Props {
-  title: string;
   numImages?: number;
   images: SlideImage[]
+  title: string
 }
 
-function ImageCarousel({ title, images, numImages = 1 }: Props) {
+export default class ImageCarousel extends React.Component<Props, object> {
+  dots: SlideDot[] = [];
+  slideIndex = 1;
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      slideIndex: 1,
+      imgs: this.props.images
+    }
+    console.log("test!" + this.props.title)
+    this.dots = this.props.images.map((SlideImage: SlideImage) => {
+      return new SlideDot(
+        {
+          active: false
+        });
+    })
 
-  let slideIndex = 1;
-  showSlides(slideIndex, images);
+    this.showSlides(
+      this.slideIndex);
+  }
 
   // Next/previous controls
-  function plusSlides(n: any) {
-    showSlides(slideIndex += n);
+  plusSlides(n: any) {
+    this.showSlides(this.slideIndex += n);
   }
 
   // Thumbnail image controls
-  function currentSlide(n: any) {
-    showSlides(slideIndex = n);
+  currentSlide(n: number) {
+    this.showSlides(this.slideIndex = n);
   }
 
-  function showSlides(n: any, images: SlideImage[] = []) {
+  showSlides(n: number) {
     var i;
-    var slides = images
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-      slides[i].visible = false;
+    // Prevent index to pass amount of images (last index)
+    if (n > this.props.images.length) {
+      this.slideIndex = 1
     }
-    slides[slideIndex - 1].visible = true;
-    console.log(slides)
+
+    if (n < 1) {
+      this.slideIndex = this.props.images.length
+    }
+
+    for (i = 0; i < this.props.images.length; i++) {
+      this.props.images[i].visible = false;
+    }
+
+    this.props.images[this.slideIndex - 1].visible = true;
+    this.setState({ imgs: this.props.images })
+  }
+  render() {
+    const { numImages, images, title } = this.props;
+    return (
+      <div className="slideshow-element">
+        <div className="slideshow-container">
+          {
+            this.props.images.map((ImageSlide: SlideImage, index) => (
+
+              <div key={index}>
+                {
+                  ImageSlide.visible ?
+                    <div key={index} className="mySlides fade">
+                      <div className="numbertext">{index} / 3</div>
+                      <img src={ImageSlide.url} alt="" />
+                      <div className="text">Caption Text</div>
+                    </div>
+                    : ""
+                }
+
+              </div>
+            ))
+          }
+          <span className="prev" onClick={() => { this.plusSlides(-1) }}>&#10094;</span>
+          <span className="next" onClick={() => { this.plusSlides(1) }}>&#x276F;</span>
+        </div>
+        <div style={{ textAlign: "center" }}>
+          {
+            this.dots.map((SlideDots: SlideDot, index) => (
+              <span key={index} className="dot" onClick={() => { this.currentSlide(index) }}></span>
+            ))}
+        </div>
+      </div>
+    )
   }
 
-  return (
-    <div className="slideshow-element">
-      <div className="slideshow-container">
-        {
-          images.map((ImageSlide: SlideImage, index) => (
-            <div key={index} className="mySlides fade" >
-              <div className="numbertext">{index} / 3</div>
-              <img src={ImageSlide.url} alt="" />
-              <div className="text">Caption Text</div>
-            </div>
-          ))
-        }
-        <span className="prev" onClick={() => { }}>&#10094;</span>
-        <span className="next" onClick={() => { }}>&#10094;</span>
-      </div>
 
-      <div style={{ textAlign: "center" }}>
-        <span className="dot" onClick={() => { }}></span>
-        <span className="dot" onClick={() => { }}></span>
-        <span className="dot" onClick={() => { }}></span>
-      </div>
-    </div>
-
-
-
-  );
-}
-
-export default ImageCarousel;
-
-// helpers
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function getImages(numImages: number) {
-  return 0;
 }
